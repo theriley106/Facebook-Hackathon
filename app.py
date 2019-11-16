c8cce7dbab80306032f1c0f3d128b391d65409a5
theriley106 <christopherlambert106@gmail.com>
2019-11-16 14:34:48 -0800

    Added twilio

--- app.py
+++ app.py
@@ -7,10 +7,21 @@ try:
 except:
     verify_token = os.getenv('VERIFY_TOKEN', None)
     access_token = os.getenv('ACCESS_TOKEN', None)
+from flask import Flask, request, redirect
+from twilio import twiml
 
 app = Flask(__name__)
 
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = twiml.Response()
 
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
 
 @app.route('/webhook', methods=['GET'])
 def webhook_verify():
